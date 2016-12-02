aa19e2aaee62d9b7774eadaa2d492dd0ccd757f6
Aliaksandr Makarau <aliaksandr_makarau@epam.com>
2016-12-02 17:36:42 +0300

    Added Chapter 08.

---
+++ Chapter08/src/runner.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int x_outside;
+double d_outside;
+
+int main()
+{
+	cout << "Chapter 8" << endl;
+
+	cout << x_outside << endl;
+	cout << d_outside << endl;
+
+	getchar();
+
+	return 0;
+}
